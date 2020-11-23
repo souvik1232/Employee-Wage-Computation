382c6ebaf93ea84c006b854b5a9d9b34c2a5cac0
souvik1232 <souvik199831@gmail.com>
2020-11-23 21:55:46 +0530

    Added:Wage calculation for given duration

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -17,28 +17,41 @@ public class EmployeeWage {
 		int FullDayHour=8;
 		int PartTimeHour=4;
 		int DailyWage=0;
+		int MAX_HRS_IN_MONTH=100;
+		int NUM_WORKING_DAYS=20;
+		int totalWorkingDays=0;
+		int totalWorkhours=0;
 		int WorkingDays=20;
 		int MonthlyWage=0;
+		int totalWage=0;
 		int EmployeeType=random.nextInt(2);
-		switch(EmployeeType)
+		while(totalWorkhours<MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS)
 		{
-		case 0:
-			DailyWage=(WagePerHour*FullDayHour);
-			System.out.println("Employee wage is: "+DailyWage);
-			MonthlyWage=(DailyWage*WorkingDays);
-			System.out.println("Monthly wage is: "+MonthlyWage);
-			break;
-		case 1:
-			DailyWage=(WagePerHour*PartTimeHour);
-			System.out.println("Employee wage is: "+DailyWage);
-			MonthlyWage=(DailyWage*WorkingDays);
-			System.out.println("Monthly wage is: "+MonthlyWage);
-			break;
-		default:
-			System.out.println("Invalid");
-				
+			switch(EmployeeType)
+			{
+			case 0:
+				DailyWage=(WagePerHour*FullDayHour);
+				//System.out.println("Employee wage is: "+DailyWage);
+				MonthlyWage=(DailyWage*WorkingDays);
+				//System.out.println("Monthly wage is: "+MonthlyWage);
+				totalWorkhours=(totalWorkhours+FullDayHour);
+				break;
+			case 1:
+				DailyWage=(WagePerHour*PartTimeHour);
+				//System.out.println("Employee wage is: "+DailyWage);
+				MonthlyWage=(DailyWage*WorkingDays);
+				//System.out.println("Monthly wage is: "+MonthlyWage);
+				totalWorkhours=(totalWorkhours+PartTimeHour);
+				break;
+			default:
+				System.out.println("Invalid");
+					
+			}
+			totalWorkingDays++;
+			
 		}
-		
+		totalWage=(totalWorkhours*totalWorkingDays*WagePerHour);
+		System.out.println("Total Wage is : "+totalWage);
 	}
 
 }
