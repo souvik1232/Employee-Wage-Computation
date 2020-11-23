d1284f2d616f2dcee3777a9a3ba0d071ab17552a
souvik1232 <souvik199831@gmail.com>
2020-11-23 20:06:20 +0530

    Added:Logic for attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,22 @@
 package com.java.Employee;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Wage Computation");
+		Random random=new Random();
+		int attendance=random.nextInt(2);
+		if(attendance == 0)
+		{
+			System.out.println("Employee is absent!");
+		}
+		else
+		{
+			System.out.println("Employee is present!");
+		}
 		
 	}
 
