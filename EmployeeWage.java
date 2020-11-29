67a08fae51a56be8637f9814e3d848a39000e1fe
souvik1232 <souvik199831@gmail.com>
2020-11-29 11:44:11 +0530

    Added:Logic for saving data Comapny wise

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,14 +5,27 @@ public class EmployeeWage {
 	
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-//	public static final int EMP_RATE_PER_HOUR = 20;
-//	public static final int NUM_OF_WORKING_DAYS = 2;
-//	public static final int MAX_HRS_IN_MONTH = 10;
 	
-	public static int Empwagecomputation(String comapny, int empRatePerHour, int numOfWokingDays, int maxHoursPerMonth)
+	private final String comapny;
+	private final int empRatePerHour; 
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+	
+	
+	
+	public EmployeeWage(String comapny, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth) {
+		
+		this.comapny = comapny;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHourPerMonth;
+	}
+
+	public void Empwagecomputation()
 	{
 		int emphrs = 0, totalemphrs = 0, totalworkingdays = 0;
-		while (totalemphrs <= maxHoursPerMonth && totalworkingdays < numOfWokingDays)
+		while (totalemphrs <= maxHoursPerMonth && totalworkingdays < numOfWorkingDays)
 		{
 			totalworkingdays++;
 			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -30,14 +43,22 @@ public class EmployeeWage {
 			totalemphrs += emphrs;
 			System.out.println("Day#: " + totalworkingdays + " Emp Hr: " +emphrs);
 		}
-	int totalEmpwage  =  totalemphrs * empRatePerHour; 
-	System.out.println("Total Emp Wage for Comapny : " +comapny+"is :" + totalEmpwage);
-	return totalEmpwage;
+		totalEmpWage  =  totalemphrs * empRatePerHour; 
 	}
 	
+	
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Comapny : " +comapny+" is : " + totalEmpWage;
+	}
+
 	public static void main(String[] args)
 	{
-		Empwagecomputation("Dmart", 20, 2, 10);
-		Empwagecomputation("Reliance", 10, 4, 20);
+		EmployeeWage dMart = new EmployeeWage("Dmart", 20, 2, 10);
+		EmployeeWage Reliance = new EmployeeWage("Reliance", 10, 4, 20);
+		dMart.Empwagecomputation();
+		Reliance.Empwagecomputation();
+		System.out.println(dMart);
+		System.out.println(Reliance);
 	}
 }
