addf60ea0156dcc2ccdaa0500189c2624aa4ec1f
souvik1232 <souvik199831@gmail.com>
2020-11-29 11:05:27 +0530

    Refractored:Ability to compute wage for Multiple Comapnies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,14 +5,14 @@ public class EmployeeWage {
 	
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MONTH = 10;
+//	public static final int EMP_RATE_PER_HOUR = 20;
+//	public static final int NUM_OF_WORKING_DAYS = 2;
+//	public static final int MAX_HRS_IN_MONTH = 10;
 	
-	public static int Empwagecomputation()
+	public static int Empwagecomputation(String comapny, int empRatePerHour, int numOfWokingDays, int maxHoursPerMonth)
 	{
 		int emphrs = 0, totalemphrs = 0, totalworkingdays = 0;
-		while (totalemphrs <= MAX_HRS_IN_MONTH && totalworkingdays < NUM_OF_WORKING_DAYS)
+		while (totalemphrs <= maxHoursPerMonth && totalworkingdays < numOfWokingDays)
 		{
 			totalworkingdays++;
 			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -30,13 +30,14 @@ public class EmployeeWage {
 			totalemphrs += emphrs;
 			System.out.println("Day#: " + totalworkingdays + " Emp Hr: " +emphrs);
 		}
-	int totalEmpwage  =  totalemphrs * EMP_RATE_PER_HOUR; 
-	System.out.println("Total Emp Wage : " + totalEmpwage);
+	int totalEmpwage  =  totalemphrs * empRatePerHour; 
+	System.out.println("Total Emp Wage for Comapny : " +comapny+"is :" + totalEmpwage);
 	return totalEmpwage;
 	}
 	
 	public static void main(String[] args)
 	{
-		Empwagecomputation();
+		Empwagecomputation("Dmart", 20, 2, 10);
+		Empwagecomputation("Reliance", 10, 4, 20);
 	}
 }
