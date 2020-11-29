7bb2ac291a29c905d4c8ffbccf26bbb5bbd77a17
souvik1232 <souvik199831@gmail.com>
2020-11-29 12:51:48 +0530

    Added:Logic for storing data in array

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,64 +1,72 @@
 package com.java.Employee;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
 	
-	private final String comapny;
-	private final int empRatePerHour; 
-	private final int numOfWorkingDays;
-	private final int maxHoursPerMonth;
-	private int totalEmpWage;
+	public int numOfCompany=0;
 	
+	CompanyEmpWage companyEmpWage[];
 	
-	
-	public EmployeeWage(String comapny, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth) {
-		
-		this.comapny = comapny;
-		this.empRatePerHour = empRatePerHour;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHoursPerMonth = maxHourPerMonth;
-	}
-
-	public void Empwagecomputation()
+	public EmployeeWage()
 	{
-		int emphrs = 0, totalemphrs = 0, totalworkingdays = 0;
-		while (totalemphrs <= maxHoursPerMonth && totalworkingdays < numOfWorkingDays)
+		companyEmpWage=new CompanyEmpWage[5];
+	}
+	
+	private void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
+	{
+		companyEmpWage[numOfCompany]=new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+		numOfCompany++;
+	}
+	
+	private void computeEmpWage()
+	{
+		for (int i = 0; i <numOfCompany; i++) 
 		{
-			totalworkingdays++;
-			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-			switch (empcheck)
-			{
-				case IS_PART_TIME:
-					emphrs = 4;
-					break;
-				case IS_FULL_TIME:
-					emphrs = 8;
-					break;
-				default:
-					emphrs = 0;
-			}
-			totalemphrs += emphrs;
-			System.out.println("Day#: " + totalworkingdays + " Emp Hr: " +emphrs);
+			companyEmpWage[i].setTotalEmpWage(this.computeEmpWage(companyEmpWage[i]));
+			System.out.println(companyEmpWage[i]);
 		}
-		totalEmpWage  =  totalemphrs * empRatePerHour; 
 	}
 	
 	
-	@Override
-	public String toString() {
-		return "Total Emp Wage for Comapny : " +comapny+" is : " + totalEmpWage;
+	private int computeEmpWage(CompanyEmpWage companyEmpWage)
+	{
+	int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+	Random random=new Random();
+	while(totalEmpHrs<=companyEmpWage.maxHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays)
+	{
+		totalWorkingDays++;
+		
+		int empCheck=random.nextInt(3);
+				//(int) Math.floor(Math.random() * 10)%3;
+		switch(empCheck)
+		{
+		case IS_PART_TIME:
+			empHrs=4;
+			break;
+			
+		case IS_FULL_TIME:
+			empHrs=8;
+			break;
+		default:
+			empHrs=0;
+		}
+		totalEmpHrs+=empHrs;
+		System.out.println("day: "+totalWorkingDays + "EmpHrs: "+empHrs);
+		}
+	
+	return totalEmpHrs * companyEmpWage.empRatePerHour;
 	}
 
 	public static void main(String[] args)
 	{
-		EmployeeWage dMart = new EmployeeWage("Dmart", 20, 2, 10);
-		EmployeeWage Reliance = new EmployeeWage("Reliance", 10, 4, 20);
-		dMart.Empwagecomputation();
-		Reliance.Empwagecomputation();
-		System.out.println(dMart);
-		System.out.println(Reliance);
+		EmployeeWage empWage = new EmployeeWage();
+		empWage.addCompanyEmpWage("Dmart", 20, 2, 10);
+		empWage.addCompanyEmpWage("Reliance", 10, 4, 20);
+		empWage.computeEmpWage();
 	}
 }
