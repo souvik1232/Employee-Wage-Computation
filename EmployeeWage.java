fd254bf58083075572fe4e7447d46605163040da
souvik1232 <souvik199831@gmail.com>
2020-12-01 08:34:20 +0530

    Refractored:Interface and overriden methods

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Random;
 
-public class EmployeeWage {
+public class EmployeeWage implements IComputeEmpWage{
 
 	
 	public static final int IS_PART_TIME=1;
@@ -24,54 +24,7 @@ public class EmployeeWage {
 		companyEmpWageMap=new HashMap<String, CompanyEmpWage>();
 	}
 	
-	private void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
-	{
-		CompanyEmpWage companyEmpWage=new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);	
-		
-		companyEmpWageList.add(companyEmpWage);
-		companyEmpWageMap.put(company, companyEmpWage);
-	}
-	
-	private void computeEmpWage()
-	{
-		for (int i = 0; i <companyEmpWageList.size(); i++) 
-		{
-			CompanyEmpWage company=companyEmpWageList.get(i);
-			company.setTotalEmpWage(this.computeEmpWage(company));
-			System.out.println(company);
-			
 
-		}
-	}
-	
-	
-	private int computeEmpWage(CompanyEmpWage companyEmpWage)
-	{
-	int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
-	Random random=new Random();
-	while(totalEmpHrs<=companyEmpWage.maxHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays)
-	{
-		totalWorkingDays++;
-		
-		int empCheck=random.nextInt(3);
-		switch(empCheck)
-		{
-		case IS_PART_TIME:
-			empHrs=4;
-			break;
-			
-		case IS_FULL_TIME:
-			empHrs=8;
-			break;
-		default:
-			empHrs=0;
-		}
-		totalEmpHrs+=empHrs;
-		System.out.println("day: "+totalWorkingDays + "EmpHrs: "+empHrs);
-		}
-	
-	return totalEmpHrs * companyEmpWage.empRatePerHour;
-	}
 
 	public static void main(String[] args)
 	{
@@ -80,6 +33,59 @@ public class EmployeeWage {
 		empWage.addCompanyEmpWage("Reliance", 10, 4, 20);
 		empWage.computeEmpWage();
 	}
+
+
+	@Override
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		CompanyEmpWage companyEmpWage=new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);	
+		
+		companyEmpWageList.add(companyEmpWage);
+		companyEmpWageMap.put(company, companyEmpWage);
+		
+	}
+
+	@Override
+	public void computeEmpWage() {
+		for (int i = 0; i <companyEmpWageList.size(); i++) 
+			{
+				CompanyEmpWage company=companyEmpWageList.get(i);
+				company.setTotalEmpWage(this.computeEmpWage(company));
+				System.out.println(company);
+			
+	
+		}
+		
+	}
+
+	@Override
+	public int computeEmpWage(CompanyEmpWage companyEmpWage) {
+		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+		Random random=new Random();
+		while(totalEmpHrs<=companyEmpWage.maxHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays)
+		{
+			totalWorkingDays++;
+			
+			int empCheck=random.nextInt(3);
+			switch(empCheck)
+			{
+			case IS_PART_TIME:
+				empHrs=4;
+				break;
+				
+			case IS_FULL_TIME:
+				empHrs=8;
+			break;
+			default:
+				empHrs=0;
+			}
+			totalEmpHrs+=empHrs;
+			System.out.println("day: "+totalWorkingDays + "EmpHrs: "+empHrs);
+			}
+		
+		return totalEmpHrs * companyEmpWage.empRatePerHour;
+	}
+
+	@Override
 	public int getTotalWage(String company) {
 		return companyEmpWageMap.get(company).totalEmpWage;
 	}
